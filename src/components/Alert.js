ee86a389c9614390140072929b3998c7681a7beb
UtkarshChaurasia <msdianuc07@gmail.com>
2021-10-09 20:18:45 +0530

    minor ui bugs fixes

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,7 +6,7 @@ function Alert(props) {
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
     return (
-        <div style={{height: '50px'}}>
+        <div style={{height: '35px'}}>
         {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
             <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
             <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
